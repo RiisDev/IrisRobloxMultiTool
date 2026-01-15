638ff871a4a872308291f861d8f5b5ab50f712bc
Iris <39096994+RiisDev@users.noreply.github.com>
2026-01-14 19:25:56 -0700

    Prep Audio Downloader

--- IrisRobloxMultiTool/Pages/APIChecker.xaml.cs
+++ IrisRobloxMultiTool/Pages/APIChecker.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System.Net.Http.Json;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+// ReSharper disable MemberHidesStaticFromOuterClass
 
 namespace IrisRobloxMultiTool.Pages
 {
@@ -20,21 +22,36 @@ namespace IrisRobloxMultiTool.Pages
         private static readonly SolidColorBrush BlueBrush = new(Color.FromRgb(66, 154, 249));
 
 		private record ServerData(TextBlock Ping, TextBlock Status);
-		private record RobloxAssetCheck(string Url, string SuccessIndicator, ServerData TextObject, Func<string, bool>? ExtraCheck = null);
+		private record RobloxAssetCheck(string Url, string SuccessIndicator, ServerData TextObject, Func<string, bool>? ExtraCheck = null, HttpType? HttpType = HttpType.Get, dynamic? PostContent = null);
 		private record GetStatus(string Status, SolidColorBrush Brush, int Ping);
 
-		private async Task<GetStatus> GetApiStatusAsync(string url, string successIndicator, ApiType apiType = ApiType.Standard, Func<string, bool>? extraCheck = null)
+		public enum HttpType
+		{
+			Get,
+			Post
+		}
+
+		private async Task<GetStatus> GetApiStatusAsync(string url, string successIndicator, ApiType apiType = ApiType.Standard, Func<string, bool>? extraCheck = null, HttpType? httpType = HttpType.Get, dynamic? postContent = null)
         {
 			try
 			{
-                TimeOnly startTime = TimeOnly.FromDateTime(DateTime.Now);
+				RobloxClient.DefaultRequestHeaders.TryAddWithoutValidation("x-csrf-token", await Roblox.RefreshCsrfToken());
 
-				string response = apiType switch
+                TimeOnly startTime = TimeOnly.FromDateTime(DateTime.Now);
+				
+				using HttpRequestMessage request = new(httpType == HttpType.Get ? HttpMethod.Get : HttpMethod.Post, url);
+
+				if (httpType == HttpType.Post && postContent != null) 
+					request.Content = JsonContent.Create(postContent);
+
+				HttpResponseMessage responseMessage = apiType switch
 				{
-					ApiType.Roblox => await RobloxClient.GetStringAsync(url),
-					ApiType.Standard => await BaseClient.GetStringAsync(url),
-					_ => throw new NotImplementedException()
+					ApiType.Roblox => await RobloxClient.SendAsync(request),
+					ApiType.Standard => await BaseClient.SendAsync(request),
+					_ => throw new InvalidOperationException()
 				};
+				
+				string response = await responseMessage.Content.ReadAsStringAsync();
 
                 TimeOnly endTime = TimeOnly.FromDateTime(DateTime.Now);
 
@@ -50,8 +67,9 @@ namespace IrisRobloxMultiTool.Pages
 
 				return new GetStatus("Blocked or Offline", RedBrush, ping);
 			}
-			catch (Exception)
+			catch (Exception ex)
 			{
+				Log(ex);
 				return new GetStatus("Blocked or Offline", RedBrush, -1);
 			}
 		}
@@ -88,24 +106,43 @@ namespace IrisRobloxMultiTool.Pages
 			        "{\"TargetId\":607785314,",
 			        new ServerData(MarketplaceApiPing, MarketplaceApiStatus)
 			    ),
-		        new (
-			        "https://search.roblox.com/catalog/contents?CatalogContext=2&Subcategory=16&Keyword=ree&SortAggregation=5&PageNumber=1&LegendExpanded=true&Category=9",
-			        "Creator Marketplace",
-			        new ServerData(AudioApiPing, AudioApiOnlineStatus)
-			    ),
                 new (
 					"https://thumbnails.roblox.com/v1/groups/icons?groupIds=3580494&size=150x150&format=Png&isCircular=false",
 					"Completed",
                     new ServerData(ThumbnailApiPing, ThumbnailApiStatus)
-	            )
-	        ];
+	            ),
+				new (
+					"https://apis.roblox.com/toolbox-service/v1/marketplace/3?limit=100&pageNumber=0&sortDirection=Descending&audioTypes=0&uiSortIntent=10",
+					"totalResults",
+					new ServerData(AudioApiPing, AudioApiOnlineStatus)
+				),
+		        new (
+					"https://apis.roblox.com/toolbox-service/v1/items/details?assetIds=94215407204609,7135127272",
+			        "{\"data\":[{\"asset\":",
+			        new ServerData(AssetDetailsPing, AssetDetailsStatus)
+		        ),
+				new (
+					"https://assetdelivery.roblox.com/v1/assets/batch",
+					"[{\"location\":",
+					new ServerData(AudioDownloadPing, AudioDownloadStatus),
+					null,
+					HttpType.Post,
+					new List<object>
+					{
+						new
+						{
+							RequestId = "94215407204609",
+							AssetId = 94215407204609L
+						}
+					})
+			];
 
             foreach (RobloxAssetCheck check in robloxApiChecks)
             {
                 SetControlToChecking(check.TextObject.Ping, check.TextObject.Status);
                 _ = Task.Run(async () =>
                 {
-                    GetStatus status = await GetApiStatusAsync(check.Url, check.SuccessIndicator, ApiType.Roblox, check.ExtraCheck);
+                    GetStatus status = await GetApiStatusAsync(check.Url, check.SuccessIndicator, ApiType.Roblox, check.ExtraCheck, check.HttpType, check.PostContent);
                     UpdateLabel(check.TextObject.Ping, $"{status.Ping}ms", PingToBrush(status.Ping));
                     UpdateLabel(check.TextObject.Status, status.Status, status.Brush);
                 });
